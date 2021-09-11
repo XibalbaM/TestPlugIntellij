b210fb2d6d3d6f774d391a825f8aa13356c0047d
GitHub Action <action@github.com>
2021-09-11 11:24:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/xibalbam/testplugintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xibalbam/testplugintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xibalbam.testplugintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xibalbam.testplugintellij.MyBundle
 
 class MyApplicationService {
 
